379ab2eaf98b7f248a5dce30d67e9a9ab3eef281
Paspy95 <146121803+Paspy95@users.noreply.github.com>
2024-05-27 17:18:49 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,19 +6,24 @@ function solveEquation(a, b, c) {
 	if (d < 0)
 		return false;
 	arr['discriminant'] = d;
+	console.log('d = ' + d)
 	if (d = 0)
 		arr['roots'] = (-b / 2 * a);
-	if (d > 0)
-		arr['roots'] = (-b + Math.sqrt(d)) / (2 * a);
-	arr['roots'] = (-b - Math.sqrt(d)) / (2 * a);
+	if (d > 0) {
+		let tmp = [];
+		tmp.push(-b + Math.sqrt(d)) / (2 * a);
+		tmp.push(-b - Math.sqrt(d)) / (2 * a);
+		arr['roots'] = tmp;
+	}
 	return arr;
 }
+console.log(solveEquation(1, 4, 4))
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let S = amount - contribution;
 	let P = percent / 100 / 12;
 	let n = countMonths;
-	let pay = S * (P + (P / (((1 + P) ^ n) - 1)));
+	let pay = S * (P + (P / (((1 + P) ** n) - 1)));
 	let totalPay = pay * countMonths;
 	totalPay = Number(totalPay.toFixed(2))
 	return totalPay;
