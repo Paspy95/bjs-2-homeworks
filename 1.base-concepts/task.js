60736bf211450b2b27f288288bbb68e3a12920f9
Paspy95 <146121803+Paspy95@users.noreply.github.com>
2024-05-29 19:31:18 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -9,7 +9,7 @@ function solveEquation(a, b, c) {
 		arr.push(-b / 2 * a);
 	if (d > 0) {
 		arr.push(-b + Math.sqrt(d) / (2 * a));
-		arr.push(-b + Math.sqrt(d) / (2 * a));
+		arr.push(-b - Math.sqrt(d) / (2 * a));
 	}
 	return arr;
 }
