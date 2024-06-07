b9ad0ea89a66279ff408255031292eeb00a82db6
Paspy95 <146121803+Paspy95@users.noreply.github.com>
2024-06-07 06:53:28 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -19,7 +19,7 @@ Student.prototype.addMarks = function(...marks) {
 
 Student.prototype.getAverage = function() {
 	if (this.marks === undefined || this.marks.length === 0) {
-		return 0;
+		return this.exclude;
 	}
 	return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
 
