6cbc0888a91a133d48bda415b19da1698b343856
Paspy95 <146121803+Paspy95@users.noreply.github.com>
2024-06-25 21:38:58 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -13,4 +13,56 @@ function validateCount(value) {
 	} catch (error) {
 		return error;
 	}
-}﻿
+}
+
+function parseCount(value) {
+	const parsedValue = Number.parseFloat(value);
+	if (Number.isNaN(parsedValue)) {
+		throw new Error("Невалидное значение");
+	}
+	return parsedValue;
+}
+
+function validateCount(value) {
+	try {
+		const parsedValue = parseCount(value);
+		return parsedValue;
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (a + b < c || a + c < b || b + c < a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+	get perimetr() {
+		return this.a + this.b + this.c;
+	}
+	get area() {
+		const p = this.perimetr / 2;
+		const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return area;
+	}
+
+}
+
+function getTringle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get perimetr() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			}
+		}
+	}
+}
