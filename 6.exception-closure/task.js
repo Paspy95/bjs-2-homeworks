bb9c3acf63377a6209984aa79a89fb27869824ec
Paspy95 <146121803+Paspy95@users.noreply.github.com>
2024-06-25 21:23:43 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+function parseCount(value) {
+	const parsedValue = Number.parseFloat(value);
+	if (Number.isNaN(parsedValue)) {
+		throw new Error("Невалидное значение");
+	}
+	return parsedValue;
+}
+
+function validateCount(value) {
+	try {
+		const parsedValue = parseCount(value);
+		return parsedValue;
+	} catch (error) {
+		return error;
+	}
+}﻿
